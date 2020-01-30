b1ace2b62ef02b1d57175cb5ead6774884f84db2
RPKQ <rpkq0512@gmail.com>
2020-01-30 20:38:19 +0800

    init normal mapped ladybug

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    devtool: "cheap-eval-source-map",
+    mode: "production",
+    context: path.resolve(__dirname, './src'),
+    entry: {
+        main: './main.js',
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js'
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendor',
+                    chunks: 'all'
+                }
+            }
+        }
+    },
+    devServer: {
+        contentBase: path.join(__dirname, './dist'),
+        compress: true,
+        hot: true,
+        port: 9000
+    },
+};
