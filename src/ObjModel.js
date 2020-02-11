32adab21e3c05aa74bdbc37eb8b29a9d27234375
RPKQ <rpkq0512@gmail.com>
2020-02-11 22:23:26 +0800

    ObjModel: dragon worked.
    now work only for idx == idx_n

--- src/ObjModel.js
+++ src/ObjModel.js
@@ -19,7 +19,12 @@ export default class ObjModel {
 	async draw() {
 		for (let i = 0; i < this.vaos.length; i++) {
 			gl.bindVertexArray(this.vaos[i]);
-			gl.drawElements(gl.TRIANGLES, this.indexCounts[i], gl.UNSIGNED_INT, 0);
+			await gl.drawElements(
+				gl.TRIANGLES,
+				this.indexCounts[i],
+				gl.UNSIGNED_INT,
+				0
+			);
 			gl.bindVertexArray(null);
 		}
 	}
@@ -29,6 +34,8 @@ export default class ObjModel {
 		let lines = await _loadFile(url);
 
 		this.idx = [];
+		this.idx_t = [];
+		this.idx_n = [];
 		this.v = [];
 		this.vn = [];
 		this.vt = [];
@@ -56,6 +63,18 @@ export default class ObjModel {
 				this.idx.push(parseInt(line[1].split("/")[0]) - 1);
 				this.idx.push(parseInt(line[2].split("/")[0]) - 1);
 				this.idx.push(parseInt(line[3].split("/")[0]) - 1);
+				if (line[1].split("/")[1]) {
+					this.idx_t.push(parseInt(line[1].split("/")[1]) - 1);
+					this.idx_t.push(parseInt(line[2].split("/")[1]) - 1);
+					this.idx_t.push(parseInt(line[3].split("/")[1]) - 1);
+				} else {
+					this.idx_t.push(parseInt(0));
+					this.idx_t.push(parseInt(0));
+					this.idx_t.push(parseInt(0));
+				}
+				this.idx_n.push(parseInt(line[1].split("/")[2]) - 1);
+				this.idx_n.push(parseInt(line[2].split("/")[2]) - 1);
+				this.idx_n.push(parseInt(line[3].split("/")[2]) - 1);
 			} else if (line[0] == "usemtl") {
 				if (line[1] == "0") this.usemtl.push(null);
 				else this.usemtl.push(line[1]);
@@ -64,6 +83,8 @@ export default class ObjModel {
 					this.indexCounts.push(this.idx.length);
 					await this.buildModel();
 					this.idx = [];
+					this.idx_n = [];
+					this.idx_t = [];
 					this.v = [];
 					this.vn = [];
 					this.vt = [];
@@ -76,12 +97,45 @@ export default class ObjModel {
 		await this.buildModel();
 		this.indexCounts.push(this.idx.length);
 		this.idx = [];
+		this.idx_n = [];
+		this.idx_t = [];
 		this.v = [];
 		this.vn = [];
 		this.vt = [];
 	}
 
 	buildModel() {
+		console.log(
+			`Build a mesh with ${this.v.length} vertices, ${this.vn.length} normals, ` +
+				`${this.vt.length} UV , ${this.idx.length} indices, ${this.idx_n.length} normal indices, ` +
+				`${this.idx_t.length} UV indices`
+		);
+		// --- convert the data with ebo --- //
+		let vn_order = new Array(this.v.length);
+		let vt_order = new Array((this.v.length / 3) * 2);
+		console.log(vn_order.length);
+		console.log(vt_order.length);
+
+		for (let i = 0; i < this.idx.length; i++) {
+			let vid = this.idx[i];
+			if (vid > this.v.length) console.error(vid);
+			let UVid = this.idx_t[i];
+			let nid = this.idx_n[i];
+			vt_order[vid * 2] = this.vt[UVid * 2];
+			vt_order[vid * 2 + 1] = this.vt[UVid * 2 + 1];
+			vn_order[vid * 3] = this.vn[nid * 3];
+			vn_order[vid * 3 + 1] = this.vn[nid * 3 + 1];
+			vn_order[vid * 3 + 2] = this.vn[nid * 3 + 2];
+		}
+
+		this.vt = [];
+		this.vn = [];
+		this.vt = vt_order;
+		this.vn = vn_order;
+
+		vt_order = [];
+		vn_order = [];
+
 		// vao
 		let vao = gl.createVertexArray();
 		gl.bindVertexArray(vao);
@@ -129,7 +183,7 @@ export default class ObjModel {
 
 		gl.bindVertexArray(null);
 		console.log(
-			`Build a mesh with ${this.v.length} vertices, ${this.vn.length} normals, ` +
+			`Order a mesh with ${this.v.length} vertices, ${this.vn.length} normals, ` +
 				`${this.vt.length} UV , ${this.idx.length} indices`
 		);
 	}
